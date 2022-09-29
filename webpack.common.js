a6b3b045fc26e9dc98f49f08aafd748d0b2e9a2a
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-30 00:07:28 +0800

    Add webpack

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // Style and css loader
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  //   Plugin
+  plugins: [
+    // HTML Webpack Plugin
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+  ],
+};
